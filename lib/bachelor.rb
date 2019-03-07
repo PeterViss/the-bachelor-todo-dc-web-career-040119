5a871420bd591d7e9dac009b01bf8439a813a112
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-03-07 21:04:20 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,17 +1,36 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
-end
+    data[season].each do |people|
+      people.each do |category, result|
+        if result == "Winner"
+          return people["name"].split(' ').first
+      end
+    end
+  end
+end  # code here
+
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, array|
+    array.each do |people|
+        if v == occupation
+          return people["name"]
+        end
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
-end
+
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestants|
+    contestants.each do |contestant|
+      if contestant[:hometown] == hometown
+        return contestant[:occupation]
+      end
+    end
+  end # code here
 end
 
 def get_average_age_for_season(data, season)
